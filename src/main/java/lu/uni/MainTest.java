75c05c6ea68d2f5e2ba9acf51a146030daf56982
Kevin SIMOES FERRAO <kevin.simoes.004@student.uni.lu>
2024-12-02 17:11:08 +0100

    Solved pom.xml conflicts + decreased number of bugs

--- src/main/java/lu/uni/MainTest.java
+++ src/main/java/lu/uni/MainTest.java
@@ -3,11 +3,14 @@ package lu.uni;
 import lu.uni.user.Client;
 import lu.uni.user.Employee;
 import lu.uni.client.Address;
+import lu.uni.client.BankAccount;
 import lu.uni.dao.ClientDAO;
 import lu.uni.database.DatabaseConnection;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.sql.Date;
 import java.util.UUID;
 import jakarta.persistence.*;
@@ -32,35 +35,31 @@ public class MainTest {
         EntityManager em = emf.createEntityManager();
         ClientDAO clientDAO = new ClientDAO(em);
 
-        Address address = new Address();
-        address.setStreetNumber(123);
-        address.setStreet("Main Street");
-        address.setZip(12345);
-        address.setCountry("Luxembourg");
+        Address address = new Address(123, "Main Street", 12345, "Luxembourg");
+        em.getTransaction().begin();
+        em.persist(address);
+        em.getTransaction().commit();
+
+        BankAccount bankAccount = new BankAccount(BigDecimal.valueOf(1000));
+        em.getTransaction().begin();
+        em.persist(bankAccount);
+        em.getTransaction().commit();
 
         Client client = new Client(
             UUID.randomUUID().toString(),
             "John Doe",
-            new Date(1990, 1, 1),
+            Date.valueOf(LocalDate.of(1990, 1, 1)),
             address
         );
-
+        client.setBankAccount(bankAccount);
         try {
             clientDAO.addClient(client);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        Employee employee = new Employee(
-            UUID.randomUUID().toString(),
-            "Alice Smith",
-            new Date(1985, 5, 15),
-            address,
-            "secure-key-123"
-        );
-
         em.getTransaction().begin();
-        em.persist(employee);
+        em.persist(client);
         em.getTransaction().commit();
 
         em.close();
