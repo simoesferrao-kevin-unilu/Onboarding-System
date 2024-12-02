e489efe03dfdd6c6587c935647be017cdd6ec7ea
teusch-shawn-unilu <shawn.teusch.001@student.uni.lu>
2024-12-02 22:41:53 +0100

    m

--- src/main/java/lu/uni/bean/ClientBean.java
+++ src/main/java/lu/uni/bean/ClientBean.java
@@ -25,6 +25,37 @@ public class ClientBean {
         database.saveClientData(client);
     }
 
-    // Getters and Setters
-    // ...
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    
 }
