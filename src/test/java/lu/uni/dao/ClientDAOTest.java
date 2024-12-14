7d137b5b7a876b2a1e41fd6595e0e445a10a01c2
Kevin SIMOES FERRAO <kevin.simoes.004@student.uni.lu>
2024-12-14 17:29:46 +0100

    pom update + junit added

---
+++ src/test/java/lu/uni/dao/ClientDAOTest.java
@@ -0,0 +1,16 @@
+package lu.uni.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ClientDAOTest {
+    
+    private ClientDAO clientDAO = new ClientDAO();
+
+    @Test
+    public void add2And2() {
+        //int result = calculator.add(2, 2);
+        //assertEquals(4, result);
+    }
+}
